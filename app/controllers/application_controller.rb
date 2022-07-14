025393126e9195f56d8d379beab3c9894622f654
Ana Zitzow <anasempai89@gmail.com>
2022-07-13 20:16:43 -0700

    commit tree d0d0921d61bc58c9dad596f37b7098bc70e32788

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+
+  private
+
+  def render_not_found(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
+  end
+
+  def render_invalid(exception)
+    render json: { error: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
